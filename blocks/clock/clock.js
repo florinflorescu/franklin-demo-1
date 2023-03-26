6748abee63e6e13cc5d9b6f11a312c41a6392821
Florin Florescu <florin.florescu@gmail.com>
2023-03-26 20:25:54 +0300

    sddf

--- blocks/clock/clock.js
+++ blocks/clock/clock.js
@@ -1,13 +1,17 @@
-function displayTime() {
-    var date = new Date();
-    var time = date.toLocaleTimeString();
-    var clock = document.getElementById('clock');
-    clock.innerHTML = time;
-  }
+const li = document.createElement('li');
+li.innerHTML = row.innerHTML;
+
+ function displayTime() {
+//     var date = new Date();
+//     var time = date.toLocaleTimeString();
+//     var clock = document.getElementById('clock');
+//     clock.innerHTML = time;
+  console.log("asdadasasda");
+   }
   
-  // display the time immediately when the page loads
-  displayTime();
+//   // display the time immediately when the page loads
+   displayTime();
   
-  // update the time every 10 seconds
-  setInterval(displayTime, 10000);
+//   // update the time every 10 seconds
+   setInterval(displayTime, 10000);
   
